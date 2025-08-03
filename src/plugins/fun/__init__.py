016a59c0df170423e2b401b630abba0dd7bd0d0d
John Richard <windowserror@163.com>
2025-08-03 22:03:24 +0800

    更新钓鱼平衡

--- src/plugins/fun/__init__.py
+++ src/plugins/fun/__init__.py
@@ -5,11 +5,15 @@ require("nonebot_plugin_orm")
 require("nonebot_plugin_value")
 require("nonebot_plugin_suggarchat")
 require("menu")
+from nonebot_plugin_orm import get_session
+from nonebot_plugin_value.api.api_balance import list_accounts
 from nonebot_plugin_value.api.api_currency import (
     get_or_create_currency,
 )
 from nonebot_plugin_value.pyd_models.currency_pyd import CurrencyData
+from nonebot_plugin_value.repository import AccountRepository
 
+from suggar_utils.config import config_manager
 from suggar_utils.value import SUGGAR_EXP_ID
 
 from . import auto, query, signin
@@ -38,3 +42,17 @@ async def init_currency():
             symbol="EXP",
         )
     )
+    if config_manager.config.reset_balance:
+        logger.warning("正在重置所有用户的余额...")
+        accounts = await list_accounts()
+        async with get_session() as session:
+            repo = AccountRepository(session)
+            for account in accounts:
+                if account.balance > 8000000:
+                    await repo.update_balance(
+                        account.id, account.balance / 1000, account.currency_id
+                    )
+        config_manager.config.reset_balance = False
+        await config_manager.save_config()
+        await config_manager.reload_config()
+    logger.info("货币初始化完成")
